670805a9968524a04b225f2a646d0567ee75d4ba
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-11-28 00:37:08 +0800

    style: Remove unused annotation

--- Spring_Project/src/main/java/com/example/demo/Model/GoogleSearcher.java
+++ Spring_Project/src/main/java/com/example/demo/Model/GoogleSearcher.java
@@ -11,39 +11,6 @@ import org.jsoup.select.Elements;
 
 public class GoogleSearcher {
 
-    // public void getSearchResult(String query) {
-    // try {
-    // String google = "https://www.google.com/search?q=";
-    // String charset = "UTF-8";
-    // String userAgent = "User-agent";
-
-    // Elements links = Jsoup
-    // .connect(google + URLEncoder.encode(query, charset))
-    // .userAgent(userAgent)
-    // .timeout(5000).get()
-    // .select("a[href]");
-
-    // for (Element link : links) {
-    // String title = link.text();
-    // String url = link.absUrl("href");
-    // if (url.indexOf("q=") == -1) {
-    // continue;
-    // }
-    // url = URLDecoder.decode(url.substring(url.indexOf("=") + 1,
-    // url.indexOf("&")), "UTF-8");
-
-    // if (!url.startsWith("http") || url.startsWith("https://support.google.com")
-    // || url.startsWith("https://accounts.google.com")) {
-    // continue; // Ads/news/etc and other crap.
-    // } else {
-    // System.out.println(title + " | " + url);
-    // }
-    // }
-    // } catch (IOException e) {
-    // e.printStackTrace();
-    // }
-    // }
-
     public ArrayList<Website> getSearchResult(String query) {
         try {
             String searchURL = "https://www.google.com/search?q=";
