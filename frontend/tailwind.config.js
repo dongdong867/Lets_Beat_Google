e5307de81ab774a9160e325fc0d8b09217dd5afc
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-22 07:48:30 +0800

    style(nuxt): Add css, fonts and layouts

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  theme: {
-    fontFamily: {},
-    extend: {
-      colors: {}
-    }
-  }
+	theme: {
+		fontFamily: {
+			sans: ['Outfit'],
+			serif: ['Outfit'],
+			mono: ['Outfit'],
+			display: ['Outfit'],
+			body: ['Outfit']
+		},
+		extend: {
+			colors: {
+				base: '#2d2d2d',
+				'base-dark': '#3f3f3f',
+				'base-light': '#d9d9d9'
+			}
+		}
+	}
 }
