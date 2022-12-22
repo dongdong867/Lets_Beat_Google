b139e0daa9d5b971bdb4bcc376b819aa90bcf8ce
dongdong <93483694+dongdong867@users.noreply.github.com>
2022-12-23 06:40:15 +0800

    chore(nuxt): Add axios, firebase plugins

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -3,12 +3,15 @@ import svgloader from 'vite-svg-loader'
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxtjs/tailwindcss', ['@nuxtjs/axios', { autoImports: ['defineStore'] }]],
 	vite: {
 		plugins: [svgloader()]
 	},
 	typescript: {
 		typeCheck: true
 	},
-	srcDir: 'src/'
+	srcDir: 'src/',
+	axios: {},
+	nitro: { present: 'firebase' },
+	css: ['~/assets/css/transition.css']
 } as NuxtConfig)
