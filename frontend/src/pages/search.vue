153ae7c298c5d48971b03b23819b207a28eadcb2
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-01-04 22:32:52 +0800

    fix: Fix api caller and add loading page

--- frontend/src/pages/search.vue
+++ frontend/src/pages/search.vue
@@ -1,52 +1,69 @@
 <template>
 	<div>
-		<div class="sticky top-0 bg-base">
-			<div class="flex place-items-end mx-[5vw] py-[2vh] gap-x-[2vw]">
-				<nuxt-link to="/"><img :src="Logo" class="w-[4vw]" /></nuxt-link>
+		<div v-if="loading == true">
+			<Coffee />
+		</div>
+		<div v-else>
+			<div class="sticky top-0 bg-base">
+				<div class="flex place-items-end mx-[5vw] py-[2vh] gap-x-[2vw]">
+					<nuxt-link to="/"><img :src="Logo" class="w-[4vw]" /></nuxt-link>
 
-				<div
-					class="w-[60vw] text-2xl bg-base-dark flex place-content-between place-items-center rounded-full"
-				>
-					<input v-model="q" class="w-[53vw] pl-[2vw] py-[0.5vh] bg-transparent" type="text" />
-					<div class="flex gap-x-[1vw]">
-						<img :src="Cross" class="w-[1vw] mr-[0.3vw] cursor-pointer" @click="clearInput" />
-						<div class="w-[0.1vh] h-[3vh] bg-[#a3a3a3]"></div>
-						<nuxt-link v-if="q.length != 0" :to="{ path: '/search', query: { q } }">
-							<img :src="Search" class="w-[2vw] mr-[1vw]" />
-						</nuxt-link>
-						<div v-else class="cursor-pointer select-none">
-							<img :src="Search" class="w-[2vw] mr-[1vw]" />
+					<div
+						class="w-[60vw] text-2xl bg-base-dark flex place-content-between place-items-center rounded-full"
+					>
+						<input
+							v-model="q"
+							class="w-[53vw] pl-[2vw] py-[0.5vh] bg-transparent"
+							type="text"
+							@keydown.enter="gotoRoute"
+						/>
+						<div class="flex gap-x-[1vw] place-items-center">
+							<img :src="Cross" class="w-[1vw] mr-[0.3vw] cursor-pointer" @click="clearInput" />
+							<div class="w-[0.1vh] h-[3vh] bg-[#a3a3a3]"></div>
+							<button v-if="q.length != 0" @click="gotoRoute">
+								<img :src="Bean" class="w-[2vw] mr-[1vw]" />
+							</button>
+							<div v-else class="cursor-pointer select-none">
+								<img :src="Bean" class="w-[2vw] mr-[1vw]" />
+							</div>
 						</div>
 					</div>
 				</div>
+				<div class="w-screen h-[0.1vh] bg-base-light"></div>
 			</div>
-			<div class="w-screen h-[0.1vh] bg-base-light"></div>
-		</div>
-		<client-only>
-			<div class="w-full h-max overflow-y-scroll bg-scroll">
-				<div v-for="result in resultList">
-					<a href="">{{ result.title }}</a>
-					<div>{{ result.score }}</div>
+			<div class="w-[90vw] h-max overflow-y-scroll bg-scroll m-auto mt-[2vh]">
+				<div v-for="result in resultList" class="mb-[4vh]">
+					<div class="truncate w-[30%] text-sm">{{ result.url }}</div>
+					<nuxt-link class="text-xl text-[#75a8fd]" :to="result.url">{{ result.title }}</nuxt-link>
 				</div>
 			</div>
-		</client-only>
+		</div>
 	</div>
 </template>
 
 <script setup>
 import Logo from '~/assets/icons/logo.svg?url'
 import Cross from '~/assets/icons/cross.svg?url'
-import Search from '~/assets/icons/search.svg?url'
+import Bean from '~/assets/icons/bean.svg?url'
 import axios from 'axios'
 
+const loading = ref(true)
 const route = useRoute()
-const results = await axios.get('http://localhost:8080/search/' + route.query.q)
-const resultList = reactive(results.data)
+const inputRoute = computed(() => route.path + '?q=' + q.value)
+let resultList = (await axios.get('http://localhost:8080/search/' + route.query.q)).data
+
+onMounted(() => (loading.value = false))
 
 const q = ref('')
 q.value = route.query.q
 
 const clearInput = () => (q.value = '')
+
+const gotoRoute = () => {
+	loading.value = true
+	useRouter().push('search?q=' + q.value)
+	window.location.reload(true)
+}
 </script>
 
 <style lang="scss" scoped></style>
