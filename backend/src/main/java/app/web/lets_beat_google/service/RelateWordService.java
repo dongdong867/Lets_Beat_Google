63342f193c7c413813e096b51046765b5a1a5f22
dongdong <93483694+dongdong867@users.noreply.github.com>
2023-01-05 10:57:49 +0800

    fix: Remove unused files

--- backend/src/main/java/app/web/lets_beat_google/service/RelateWordService.java
+++
@@ -1,7 +0,0 @@
-package app.web.lets_beat_google.service;
-
-import org.springframework.stereotype.Service;
-
-public interface RelateWordService {
-
-}
--- backend/src/main/java/app/web/lets_beat_google/service/impl/RelateWordServiceImpl.java
+++
@@ -1,7 +0,0 @@
-package app.web.lets_beat_google.service.impl;
-
-import app.web.lets_beat_google.service.RelateWordService;
-
-public class RelateWordServiceImpl implements RelateWordService {
-
-}
